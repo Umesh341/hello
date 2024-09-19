c175b8d9df28667198ba4ca2bbc3c6b72796d46b
Umesh Shrestha <98099220+Umesh341@users.noreply.github.com>
2024-09-19 18:42:56 +0545

    Update page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -3,7 +3,7 @@ function about() {
   return (
     <div>About
 
-<button onClick={(e)=>router.push(`/home`)}>go to home page</button>
+<button onClick={(e)=>router.push(`/`)}>go to home page</button>
     </div>
   )
 }
